2dc19b36d158c9e018f2f253841cc1aabb48c5cd
Saransh Sharma <saransh1337@gmail.com>
2021-07-30 15:23:36 +0530

    add trip

---
+++ Trip.py
@@ -0,0 +1,14 @@
+class Trip:
+
+    def __init__(self, __tripID, __date, __time, __from, __to):
+        self.__tripID = __tripID
+        self.__date = __date
+        self.__time = __time
+        self.__from = __from
+        self.__to = __to
+
+    def getTrip(self):
+        pass
+
+    def trip(self, trip):
+        pass
