f06cf9ad890b74b6f9a88f0d95316a004206aea1
Saransh Sharma <saransh1337@gmail.com>
2021-07-30 15:19:18 +0530

    add compute fare

---
+++ computeFare.py
@@ -0,0 +1,20 @@
+class ComputeFare:
+
+
+    def __init__(self, __tigerID, __tripHistory, __currentTrip):
+        self.__tigerID = __tigerID
+        self.__tripHistory = __tripHistory
+        self.__currentTrip = __currentTrip
+
+    def dayCost(self):
+        pass
+
+    def weekCost(self):
+        pass
+
+    def totalCost(self):
+        pass
+    
+    def dayCost(self):
+        pass
+    
